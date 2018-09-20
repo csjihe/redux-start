6296f50c9f0dc26ed5761bda28e6f3b70a2c96ce
spmonkey <csjihe@gmail.com>
2018-09-20 17:32:34 -0400

    react-redux switch in reducer finished

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+    counter : 0
+}
+
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUBSTRACT':
+            return {
+                counter: state.counter - action.val
+            }
+    }
+    return state;
+};
+
+export default reducer;
